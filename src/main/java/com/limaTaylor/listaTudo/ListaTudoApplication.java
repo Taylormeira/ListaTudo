83c6bfe50802e0f1f8be0e042752fd5c9467b1b1
Taylormeira <taylortmeira25@gmail.com>
2024-07-24 20:36:41 -0300

    iniciando projeto

---
+++ src/main/java/com/limaTaylor/listaTudo/ListaTudoApplication.java
@@ -0,0 +1,13 @@
+package com.limaTaylor.listaTudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaTudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaTudoApplication.class, args);
+	}
+
+}
